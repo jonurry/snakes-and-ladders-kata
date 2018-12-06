7d4af34efa9d4247bb8face331c0710b93c1a848
Jon Urry <jon@urry.me>
2018-12-06 15:56:06 +0000

    It should randomise dice rolls

--- __tests__/game.js
+++ __tests__/game.js
@@ -1,13 +1,10 @@
-// Token Can Move Across the Board
-//
-// As a player
-// I want to be able to move my token
-// So that I can get closer to the goal
-
 import Game from '../src/game'
 
 describe('Game', () => {
   describe('Token can move across the board', () => {
+    // As a player
+    // I want to be able to move my token
+    // So that I can get closer to the goal
     let game
     beforeEach(() => {
       game = new Game()
@@ -36,6 +33,23 @@ describe('Game', () => {
       game.move(3)
       game.move(4)
       expect(game.position).toEqual(8)
+    })
+  })
+
+  describe('Moves Are Determined By Dice Rolls', () => {
+    // As a player
+    // I want to move my token based on the roll of a die
+    // So that there is an element of chance in the game
+    let game
+    beforeEach(() => {
+      game = new Game()
+    })
+
+    test('It should randomise dice rolls', () => {
+      const random = jest.spyOn(Math, 'random')
+      game.rollDie()
+      expect(Math.random).toHaveBeenCalled()
+      random.mockRestore()
     })
   })
 })
--- src/game.js
+++ src/game.js
@@ -1,8 +1,17 @@
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
 export default class Game {
   constructor() {
     this.position = 1
   }
   move(spaces) {
     this.position += spaces
+  }
+  rollDie() {
+    this.position += getRandomIntInclusive(1, 6)
   }
 }
