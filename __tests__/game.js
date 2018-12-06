135cacf03bc5c937dc7937ceee4206611aa81b8f
Jon Urry <jon@urry.me>
2018-12-06 15:34:35 +0000

    Token moves are incremental
    Given the token is on square 1
    When the token is moved 3 spaces
    And then it is moved 4 spaces
    Then the token is on square 8

--- __tests__/game.js
+++ __tests__/game.js
@@ -1,35 +1,41 @@
 // Token Can Move Across the Board
-
+//
 // As a player
 // I want to be able to move my token
 // So that I can get closer to the goal
-
-// Given the game is started
-// When the token is placed on the board
-// Then the token is on square 1
-
-// Given the token is on square 1
-// When the token is moved 3 spaces
-// Then the token is on square 4
-
-// Given the token is on square 1
-// When the token is moved 3 spaces
-// And then it is moved 4 spaces
-// Then the token is on square 8
 
 import Game from '../src/game'
 
 describe('Game', () => {
   describe('Token can move across the board', () => {
+    let game
+    beforeEach(() => {
+      game = new Game()
+    })
+
     test('Token starts on square 1', () => {
-      const game = new Game()
+      // Given the game is started
+      // When the token is placed on the board
+      // Then the token is on square 1
       expect(game.position).toEqual(1)
     })
 
     test('Token can move 3 squares', () => {
-      const game = new Game()
+      // Given the token is on square 1
+      // When the token is moved 3 spaces
+      // Then the token is on square 4
       game.move(3)
       expect(game.position).toEqual(4)
+    })
+
+    test('Token moves are incremental', () => {
+      // Given the token is on square 1
+      // When the token is moved 3 spaces
+      // And then it is moved 4 spaces
+      // Then the token is on square 8
+      game.move(3)
+      game.move(4)
+      expect(game.position).toEqual(8)
     })
   })
 })
